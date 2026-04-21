4d9d215ca8c56a0d3ad25d716f7ae49d28f04728
allanMartin <z54360907@gmail.com>
2026-04-21 10:53:30 +0700

    init project

---
+++ src/test/java/com/kynn/reevo_backend/ReevoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kynn.reevo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReevoBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
